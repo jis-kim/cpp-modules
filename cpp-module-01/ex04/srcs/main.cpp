29bcfe9e7856b247d4235859e9209d0f092f64a2
Jisu Kim <hellojs242@gmail.com>
2022-06-27 22:05:18 +0900

    writing ex04

--- cpp-module-01/ex04/srcs/main.cpp
+++ cpp-module-01/ex04/srcs/main.cpp
@@ -5,4 +5,19 @@
  * @brief
  */
 
-int main(int argc, char **argv) { return 0; }
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  if (argc != 4) {
+    std::cout << "Usage: <filename> <s1> <s2>\n";
+  }
+  try {
+    std::ifstream file(argv[1]);  // constructor
+
+  } catch (std::exception &e) {
+    e.what();
+  }
+
+  return 0;
+}

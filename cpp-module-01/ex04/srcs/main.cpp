78db25030d0c4b6c7f55cb9d42cc709abf274a51
Jisu Kim <hellojs242@gmail.com>
2022-06-28 22:15:54 +0900

    give me replace

--- cpp-module-01/ex04/srcs/main.cpp
+++ cpp-module-01/ex04/srcs/main.cpp
@@ -11,13 +11,24 @@
 int main(int argc, char **argv) {
   if (argc != 4) {
     std::cout << "Usage: <filename> <s1> <s2>\n";
+    return EXIT_FAILURE;
   }
+  if (!*argv[1] || !*argv[2] || !*argv[3]) {
+    std::cout << "Invalid Arguments: \n";
+    return EXIT_FAILURE;
+  }
+  std::ifstream file(argv[1]);  // constructor
+  std::string buf;
   try {
-    std::ifstream file(argv[1]);  // constructor
+    file.seekg(0, std::ios::end);
+    int size = file.tellg();
+    buf.resize(size);
+    file.seekg(0, std::ios::beg);  // 다시 돌려놔야 함!!
+    file.read(&buf[0], size);
 
-  } catch (std::exception &e) {
-    e.what();
+    buf.find(argv[2], 0);
+  } catch (std::ifstream::failure e) {
+    std::cout << "Something Wrong: " << e.what() << '\n';
+    return EXIT_FAILURE;
   }
-
-  return 0;
 }

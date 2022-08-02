e1af92bc564e822720174ea52322a89cee805a82
Jisu Kim <hellojs242@gmail.com>
2022-08-02 22:50:14 +0900

    06

---
+++ cpp-module-06/ex02/srcs/Base.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file Base.cpp
+ * @author Jisu Kim (hellojs242@gmail.com)
+ * @date 2022-08-02
+ * @brief
+ */
+
+#include "Base.hpp"
+
+Base::~Base() {}
--- cpp-module-06/ex02/srcs/main.cpp
+++ cpp-module-06/ex02/srcs/main.cpp
@@ -14,23 +14,60 @@
 #include "C.hpp"
 
 Base *generate(void) {
-  static bool flag = true;
-
-  if (flag) {
-    srand(time(NULL));
-    flag = false;
-  }
   int randValue = rand() % 3;
   switch (randValue) {
     case 0:
       return new A();
     case 1:
       return new B();
+    default:
+      return new C();
   }
 }
 
-void identify(Base *p) {}
+void identify(Base *p) {
+  if (dynamic_cast<A *>(p)) {
+    std::cout << "A\n";
+    return;
+  }
+  if (dynamic_cast<B *>(p)) {
+    std::cout << "B\n";
+    return;
+  }
+  if (dynamic_cast<C *>(p)) {
+    std::cout << "C\n";
+    return;
+  }
+  std::cout << "What are you doing now? Stop it\n";
+}
 
-void identiry(Base &o) {}
+void identify(Base &o) {
+  try {
+    (void)dynamic_cast<A &>(o);
+    std::cout << "A\n";
+    return;
+  } catch (std::exception &e) {
+  }
+  try {
+    (void)dynamic_cast<B &>(o);
+    std::cout << "B\n";
+    return;
+  } catch (std::exception &e) {
+  }
+  try {
+    (void)dynamic_cast<C &>(o);
+    std::cout << "C\n";
+    return;
+  } catch (std::exception &e) {
+    std::cout << "What are you doing now? Stop it\n";
+  }
+}
 
-int main(void) {}
+int main(void) {
+  srand(time(NULL));
+  Base *random = generate();
+  identify(random);
+  identify(*random);
+
+  delete random;
+}

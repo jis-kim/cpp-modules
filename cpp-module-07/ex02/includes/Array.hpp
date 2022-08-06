94dae3b9c5da9cf6c8288487c06a90edf2d2303f
Jisu Kim <hellojs242@gmail.com>
2022-08-06 18:38:47 +0900

    maybe fin

--- cpp-module-07/ex02/includes/Array.hpp
+++ cpp-module-07/ex02/includes/Array.hpp
@@ -8,6 +8,8 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <stdexcept>
+
 template <typename T>
 class Array {
  private:
@@ -17,16 +19,12 @@ class Array {
  public:
   Array(void) : elements_(NULL), size_(0) {}
 
-  Array(const Array& src) : size_(src.size_) { elements = new T[src.size_]; }
+  Array(const Array& src) : size_(src.size_) { elements_ = new T[src.size_]; }
 
-  Array(const unsigned int n) : size_(n) {
-    elements = new T[n];
-    for (unsigned int i = 0; i < rhs.size_; ++i)
-      elements_[i] = rhs.elements_[i];
-  }
+  Array(const unsigned int n) : size_(n) { elements_ = new T[n]; }
 
   Array& operator=(const Array& rhs) {
-    if (elements) {
+    if (elements_) {
       delete[] elements_;
     }
     elements_ = new T[rhs.size_];
@@ -36,10 +34,14 @@ class Array {
   }
 
   const T& operator[](const unsigned int index) const {
+    if (index >= size_) throw std::out_of_range("index is out of range");
     return elements_[index];
   }
 
-  T& operator[](const unsigned int index) { return elements_[index]; }
+  T& operator[](const unsigned int index) {
+    if (index >= size_) throw std::out_of_range("index is out of range");
+    return elements_[index];
+  }
 
   unsigned int size(void) const { return size_; }
 
--- cpp-module-07/ex02/srcs/main.cpp
+++ cpp-module-07/ex02/srcs/main.cpp
@@ -1,6 +1,54 @@
-/**
- * @file main.cpp
- * @author Jisu Kim (hellojs242@gmail.com)
- * @date 2022-08-04
- * @brief test my own template
- */
+#include <Array.hpp>
+#include <iostream>
+
+#define MAX_VAL 750
+
+void test(void) {
+  Array<int> numbers(UINT_MAX - 3);
+  int* mirror = new int[MAX_VAL];
+  srand(time(NULL));
+  for (int i = 0; i < MAX_VAL; i++) {
+    const int value = rand();
+    numbers[i] = value;
+    mirror[i] = value;
+  }
+  // SCOPE
+  {
+    Array<int> tmp = numbers;
+    Array<int> test(tmp);
+  }
+
+  for (int i = 0; i < MAX_VAL; i++) {
+    if (mirror[i] != numbers[i]) {
+      std::cerr << "didn't save the same value!!" << std::endl;
+      exit(1);
+    }
+  }
+  try {
+    numbers[-2] = 0;
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+  }
+  try {
+    numbers[MAX_VAL] = 0;
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+  }
+
+  for (int i = 0; i < MAX_VAL; i++) {
+    numbers[i] = rand();
+  }
+  delete[] mirror;
+
+  Array<char> charArray(100);
+  for (int i = 0; i < 100; ++i) charArray[i] = rand() % ('Z' - 'A') + 'A';
+
+  for (int i = 0; i < 100; ++i) std::cout << charArray[i];
+  std::cout << '\n';
+}
+
+int main(int, char**) {
+  test();
+  system("leaks arrayTest");
+  return 0;
+}
